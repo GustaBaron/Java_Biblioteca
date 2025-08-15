f99914f9b3c3e1f205fe3af796fdaaee92068022
GUSTAVO ALEXANDRE DE SOUZA <gustavo.souza23@univille.br>
2025-08-15 20:12:32 -0300

    Desenvolvendo o sistema de uma biblioteca

---
+++ src/main/java/com/Gustavo_Alexandre/App.java
@@ -0,0 +1,13 @@
+package com.Gustavo_Alexandre;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/Java_Biblioteca";
+        String user = "root"; //Verificar root da maquina em uso
+        String password = "univille"; //verificar Senha do banco em uso
+
+    }
+}
